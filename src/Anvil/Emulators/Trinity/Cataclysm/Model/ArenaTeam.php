11d0141bd603b1299783ce48278b4a39bf0231ba
maverfax <maverfax@gmail.com>
2013-05-24 16:52:19 -0500

    Added arena team scopes.

--- src/Anvil/Emulators/Trinity/Cataclysm/Model/ArenaTeam.php
+++ src/Anvil/Emulators/Trinity/Cataclysm/Model/ArenaTeam.php
@@ -1,6 +1,7 @@
 <?php namespace Anvil\Emulators\Trinity\Cataclysm;
 
 use Anvil\Emulators\Eloquent\Model;
+use Anvil\Emulators\Eloquent\Builder as Query;
 
 class ArenaTeam extends Model {
 
@@ -37,5 +38,38 @@ class ArenaTeam extends Model {
 		$character->setRealm($this->getRealm())
 			->where('guid', $this->attributes['captainGuid'])
 			->first();
+	}
+
+	/**
+	 * Get only twos teams.
+	 *
+	 * @param  \Anvil\Emulators\Eloquent\Builder  $query
+	 * @return \Anvil\Emulators\Eloquent\Builder
+	 */
+	public function scopeTwos(Query $query)
+	{
+		return $query->where('type', 2);
+	}
+
+	/**
+	 * Get only threes teams.
+	 *
+	 * @param  \Anvil\Emulators\Eloquent\Builder  $query
+	 * @return \Anvil\Emulators\Eloquent\Builder
+	 */
+	public function scopeThrees(Query $query)
+	{
+		return $query->where('type', 3);
+	}
+
+	/**
+	 * Get only fives teams.
+	 *
+	 * @param  \Anvil\Emulators\Eloquent\Builder  $query
+	 * @return \Anvil\Emulators\Eloquent\Builder
+	 */
+	public function scopeFives(Query $query)
+	{
+		return $query->where('type', 5);
 	}
 }
